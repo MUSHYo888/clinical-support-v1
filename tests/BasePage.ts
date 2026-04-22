b21ed0f2699b7f80236b0b4423f055d046ae5819
MUSHYo888 <142852416+MUSHYo888@users.noreply.github.com>
2026-04-22 10:56:53 +0300

    asdfghuio

--- tests/BasePage.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test';
-
-export class BasePage {
-  constructor(public readonly page: Page) {}
-
-  async waitForNetworkIdle() {
-    await this.page.waitForLoadState('networkidle');
-  }
-}
